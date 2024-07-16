2443084d70dd4adf46a1de9aba58f55a3e07d166
sukonapa <konapallisuresh@gmail.com>
2024-07-16 13:12:12 +0800

    test cases

--- src/test/java/com/employee/net/EmployeeServiceApplicationTests.java
+++ src/test/java/com/employee/net/EmployeeServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.employee.net;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// add test cases
 @SpringBootTest
 class EmployeeServiceApplicationTests {
 
